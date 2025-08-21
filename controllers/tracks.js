80d9b10aa5cf7876bc1c16f683b9e50037543342
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-08-21 12:07:03 -0500

    update and delete added, need to be tested

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -16,20 +16,20 @@ router.post('/', async (req, res) => {
 
 // GET - INDEX - /tracks
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     try {
         const indexTracks = await Track.find();
         res.status(200).json(indexTracks);
 
     } catch (err) {
         res.status(500).json({ err: err.message });
-    } 
+    }
 });
 
 // GET - SHOW - /tracks/:id
 
 
-router.get('/:Id', async (req,res) => {
+router.get('/:Id', async (req, res) => {
     try {
         const showTrack = await Track.findById(req.params.Id);
         if (!showTrack) {
@@ -41,16 +41,53 @@ router.get('/:Id', async (req,res) => {
         if (res.statusCode === 404) {
             res.json({ err: err.message });
         } else {
-                    res.status(500).json({ err: err.message });
+            res.status(500).json({ err: err.message });
         }
-    } 
+    }
 });
 
 
 // PUT - UPDATE - /tracks/:id
+//////NEEDS TO BE TESTED//////
+
+router.put('/:Id', async (req, res) => {
+    //   res.json({ message: `Update route with the param ${req.params.Id}` });
+    try {
+
+        const updatedTrack = await Track.findByIdAndUpdate(req.params.Id, req.body, {
+        new: true,
+        });
+        if (!updatedTrack) {
+            res.status(404);
+            throw new Error('Track not found'.);
+        }
+    } catch (eer) {
+        if (res.statusCode === 404) {
+            res.json({ err: err.message });
+        } else {
+            res.status(500).json({ err: err.message });
+        }
+
+    }
+    
+
+});
+
 
 // DELETE - DELETE - /tracks/:id
+//////NEEDS TO BE TESTED//////
+router.delete('/:Id', async (req, res) => {
+    try {
+        throw new Error('this is a test error');
 
+    } catch (err) {
+        if (res.statusCode === 404) {
+            res.json({ err: err.message });
+        } else {
+            res.status(500).json({ err: err.message });
+        }
+    }
+})
 
 
 
